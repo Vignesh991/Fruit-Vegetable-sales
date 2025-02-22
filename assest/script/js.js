e5049bada940f60314b36ea791446e9834e5fc0e
Vignesh991 <vigneshbaskaran2304@gmail.com>
2025-02-22 13:49:16 +0530

    first commit

---
+++ assest/script/js.js
@@ -0,0 +1,43 @@
+//responsive//
+$('.response-slide').slick({
+    dots: true,
+    infinite: true,
+    speed: 200,
+    autoplay:true,
+    autoplaySpeed:3500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  
+ // year//
+ const d = new Date()
+ document.getElementById("demo").innerHTML = d.getFullYear();
